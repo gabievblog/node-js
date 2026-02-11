17725ec64b7187a84ad44103b0e01b94330fa5c5
gabievblog <gabriely.buzato@loglabdigital.com.br>
2026-02-11 10:36:32 -0800

    Criando Controller na app

---
+++ src/app/controllers/CustomersController.js
@@ -0,0 +1,72 @@
+class CustomersController {
+    
+    constructor() {
+        this.customers = [
+            {id: 1, name: "Gabe", email: "gabe@email.com"},
+            {id: 2, name: "Alice", email: "alice@email.com"},
+            {id: 3, name: "Bob", email: "bob@email.com"}
+        ];
+
+        this.index = this.index.bind(this);
+        this.show = this.show.bind(this);
+        this.create = this.create.bind(this);
+        this.update = this.update.bind(this);
+        this.destroy = this.destroy.bind(this);
+    }
+
+    //Listagem dos registros
+    index  (req, res)  {
+        return res.json(this.customers);
+    }
+    
+    // Recupera 1 registro
+    show  (req, res)  {
+        const id = parseInt(req.params.id);
+        const customer = this.customers.find(item => item.id === id);
+        const status = customer ? 200 : 404;
+
+        console.log("GET::/customers/:id", customer); 
+
+        return res.status(status).json(customer);
+    }
+
+    // Cria um novo registro
+    create  (req, res)  {
+        const {name, email} = req.body;
+        const nextId = this.customers[this.customers.length - 1].id + 1;
+
+        const newCustomer = {id: nextId, name, email};
+        this.customers.push(newCustomer);
+
+        return res.status(201).json(newCustomer);
+    }
+    
+    //Atualiza um registro
+    update (req, res)  {
+        const id = parseInt(req.params.id);
+        const {name, email} = req.body;
+
+        const index = this.customers.findIndex(item => item.id === id); 
+        const status = index >=0 ? 200 : 404;
+
+        if (index >= 0){
+            this.customers[index] = {id: parseInt(id), name, email};
+        }
+        return res.status(status).json(this.customers[index]);
+    }
+
+    //Exclui um registro 
+    destroy (req, res) {
+        const id = parseInt(req.params.id);
+        const index = this.customers.findIndex(item => item.id === id);
+        const status = index >=0 ? 200 : 404;
+        
+        if(index >= 0){
+            this.customers.splice(index, 1);
+        }
+        
+        return res.status(status).json({message: "Cliente deletado com sucesso!"});
+    }
+}
+
+module.exports = new CustomersController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
- const {Router} = require("express");
+const {Router} = require("express");
 
- const routes = new Router();
+const routes = new Router();
 
-routes.get("/", (req, res) => {
-    return res.json({message: "Hello!"});
-});
+const customers = require("./app/controllers/CustomersController"); //Importando o controller
+
+routes.get("/customers", customers.index);
+routes.get("/customers/:id", customers.show);
+routes.post("/customers", customers.create);
+routes.put("/customers/:id", customers.update);
+routes.delete("/customers/:id", customers.destroy);
 
 module.exports = routes; //Exportando as rotas para serem importadas em outros arquivos, como o app.js
